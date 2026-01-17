0d1f095ff9be769f76ad96575d8af97da2e3a69f
Marcus Coelho <qaizencoelho@gmail.com>
2026-01-17 11:56:40 -0300

    Refactoring Image Cropper into a component

---
+++ frontend/src/components/cropper/index.ts
@@ -0,0 +1,6 @@
+export { UploadArea } from "./UploadArea";
+export { FileInfoBar } from "./FileInfoBar";
+export { ControlPanel } from "./ControlPanel";
+export { CropArea } from "./CropArea";
+export { CropActions } from "./CropActions";
+export { PreviewResult } from "./PreviewResult";
